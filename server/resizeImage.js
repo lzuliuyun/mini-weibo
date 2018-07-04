f0e5659ff49b2356967912be4e03c332a60e793b
lzuliuyun <lzuliuyun@gmail.com>
2018-07-05 01:48:56 +0800

    update resize image

--- server/resizeImage.js
+++ server/resizeImage.js
@@ -10,35 +10,37 @@ function cryptStr (str) {
   return md5.update(str).digest('hex')
 }
 
+function outputImage (thumbPath, res) {
+  var content = fs.readFileSync(thumbPath, 'binary')
+  res.writeHead(200, 'Ok')
+  res.write(content, 'binary')
+  res.end()
+}
+
 module.exports = {
   resize (req, res, next) {
     var result = new RequestSplitter(req.path, req.query)
     var options = result.mapOptions()
-
     res.type(`image/${options.format || 'png'}`)
 
     var thumbPath = path.join(__dirname, '../' + config.imageStorage.thumb) + cryptStr(req.path) + '.' + options.format
     if (fs.existsSync(thumbPath)) {
-      // 格式必须为 binary 否则会出错
-      var content = fs.readFileSync(thumbPath, 'binary')
-      res.writeHead(200, 'Ok')
-      res.write(content, 'binary')
-      res.end()
+      outputImage(thumbPath, res)
     } else {
-      this.resizeImage(options).pipe(res)
+      this.resizeImage(options, thumbPath, res).then(() => {
+        outputImage(thumbPath, res)
+      })
     }
   },
-  resizeImage (options) {
+  resizeImage (options, thumbPath) {
     // http://localhost:3000/images/upload/0a72cec0-7f44-11e8-9260-c144568577bd.jpeg
     let imagePath = path.join(__dirname, '../' + options.imagefile)
 
-    const readStream = fs.createReadStream(imagePath)
-    let transform = sharp()
+    let transform = sharp(imagePath)
     transform = transform.toFormat(options.format)
     let width = (options.width && parseInt(options.width)) || undefined
     let height = (options.height && parseInt(options.height)) || undefined
     transform = transform.resize(width, height)
-
-    return readStream.pipe(transform)
+    return transform.toFile(thumbPath)
   }
 }
